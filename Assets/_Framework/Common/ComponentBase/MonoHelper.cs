4421cd9482257031b8574341139b5014d733d3c2
liuzhihai <liumir@dingtalk.com>
2021-03-22 17:22:19 +0800

    [新增功能]引入组件开发机制

---
+++ Assets/_Framework/Common/ComponentBase/MonoHelper.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public static class MonoHelper
+{
+    public static T AddWorldObject<T>(this MonoBehaviour monoBehaviour, T com) where T:WorldObject
+    {
+        com.GameObj = monoBehaviour.gameObject;
+        return com;
+    }
+}
---
+++ Assets/_Framework/Common/ComponentBase/WorldComponent.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class WorldComponent
+{
+    public GameObject gameObject { get; set; }
+}
